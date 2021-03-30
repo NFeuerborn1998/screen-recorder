c481f1fb20fc6125778bb9bacf1753f7c45fb3df
NFeuerborn1998 <feuerborn482401@avila.edu>
2021-03-30 18:15:21 -0500

    Update screenRecord.py

--- screenRecord.py
+++ screenRecord.py
@@ -1,31 +1,22 @@
-import time
-import numpy
+import cv2
+import numpy as np
+import pyautogui
 
-from screen_recorder_sdk import screen_recorder
+SCREEN_SIZE = pyautogui.size()
+fourcc=cv2.VideoWriter_fourcc(*"XVID")
+out = cv2.VideoWriter("output.avi", fourcc, 30, (SCREEN_SIZE))
 
+cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Live", 480, 270)
 
-def main ():
-    screen_recorder.enable_dev_log ()
-    
-    params = screen_recorder.RecorderParams ()
-    # params.pid = 0 # use it to set process Id to capture
-    # params.desktop_num = 0 # use it to set desktop num, counting from 0
+while True:
+    img = pyautogui.screenshot()
+    frame = np.array(img)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    out.write(frame)
+    cv2.imshow("Live", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
 
-    screen_recorder.init_resources (params)
-
-    screen_recorder.get_screenshot (5).save ('test_before.png')
-
-    screen_recorder.start_video_recording ('video1.mp4', 30, 8000000, True)
-    time.sleep (5)
-    screen_recorder.get_screenshot (5).save ('test_during_video.png')
-    time.sleep (5)
-    screen_recorder.stop_video_recording ()
-
-    screen_recorder.start_video_recording ('video2.mp4', 30, 8000000, True)
-    time.sleep (5)
-    screen_recorder.stop_video_recording ()
-
-    screen_recorder.free_resources ()
-
-if __name__ == "__main__":
-    main ()
+cv2.destroyAllWindows()
+out.release()
