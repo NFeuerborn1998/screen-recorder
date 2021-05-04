23f9a94e0e2197a170e19c58c65669f7054b5400
NFeuerborn1998 <feuerborn482401@avila.edu>
2021-05-03 21:00:57 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -18,17 +18,17 @@ import ffmpeg
 
 class Timer:
 
-    def __init__(self):              #initializes the class
+    def __init__(self):              
+        #initializes the class
         self.start_time = 0
         self.timing = None
         self.sec = 0
         self.holdTime = 0
         self.time = 0
 
-    def timeDisplay(self):           #Starts displaying the amount of time from the start of the recording on the timer
-
+    def timeDisplay(self):           
+        #Starts displaying the amount of time from the start of the recording on the time
         self.sec = time.time() - self.start_time
-
 
         mins = self.sec // 60
         self.sec = self.sec % 60
@@ -36,8 +36,10 @@ class Timer:
         mins = mins % 60
         timerLabel.config(text = "Recording Time: {0}:{1}:{2}".format(int(hours),int(mins),int(self.sec)))
         self.timing = timerLabel.after(1000, self.timeDisplay)
+        #https://www.geeksforgeeks.org/python-after-method-in-tkinter/
 
-    def continueDisplay(self):           #continues displaying the amount of time from the start of the recording on the timer adding the old time
+    def continueDisplay(self):           
+        #continues displaying the amount of time from the start of the recording on the timer adding the old time
 
         self.sec = time.time() - self.start_time + self.holdTime
 
@@ -47,30 +49,37 @@ class Timer:
         mins = mins % 60
         timerLabel.config(text = "Recording Time: {0}:{1}:{2}".format(int(hours),int(mins),int(self.sec)))
         self.timing = timerLabel.after(1000, self.continueDisplay)
+        #https://www.geeksforgeeks.org/python-after-method-in-tkinter/
 
-    def setStartTime(self):            #sets the start time of the recording for future use
+    def setStartTime(self):           
+         #sets the start time of the recording for future use
         start_time = self.getStartTime()
         if(start_time == 0):
             self.start_time = time.time()
     
-    def getStartTime(self):            #gets the start time of the recording
+    def getStartTime(self):            
+        #gets the start time of the recording
         return self.start_time
 
-    def restartStartTime(self):        #restarts the start time and stops the timer when stopping recording
+    def restartStartTime(self):        
+        #restarts the start time and stops the timer when stopping recording
         self.start_time = 0
         timerLabel.after_cancel(self.timing)
         self.timing = None
         self.holdTime = 0
         timerLabel['text'] = "Recording Time: 0:0:0"
 
-    def setOldTime(self):              #holds the time for future use on the timer until it is reset
+    def setOldTime(self):              
+        #holds the time for future use on the timer until it is reset
         self.holdTime += time.time() - self.start_time
 
-    def pauseTime(self):                #pauses the timer and sets the start time to 0
+    def pauseTime(self):                
+        #pauses the timer and sets the start time to 0
         self.start_time = 0
         timerLabel.after_cancel(self.timing)
 
-t = Timer()          #sets up the class for use
+t = Timer()          
+#sets up the class for use
 
 
 
@@ -79,21 +88,26 @@ fourcc=cv2.VideoWriter_fourcc(*"XVID")
 out = cv2.VideoWriter()
 video =cv2.VideoCapture(0) 
 top = Tk()
-top.title("Screen Recorder")       #sets the title of the window
-top.geometry("800x325")           #sets size of the window
+top.title("Screen Recorder")       
+#sets the title of the window
+top.geometry("800x325")           
+#sets size of the window
 photo = PhotoImage(file = "icons/icon.png")
 top.iconphoto(False, photo)
 
 
 ##################Record Options######################
 
-recordOptions = Frame(top, width = 130, height = 180, bg = "gray85", highlightbackground="black", highlightthickness=1)     #creates a background frame to group the recording options together
+recordOptions = Frame(top, width = 130, height = 180, bg = "gray85", highlightbackground="black", highlightthickness=1)     
+#creates a background frame to group the recording options together
 recordOptions.place(x = 28, y = 45)
 
-recordLabel = Label(top, text = "Record Options", font = "bold")          #creates the label text Record Option
+recordLabel = Label(top, text = "Record Options", font = "bold")          
+#creates the label text Record Option
 recordLabel.place(x = 35, y = 20)
 
-def onRecordClick():                              #changes the recording button between starting and stopping, as well as starts or stops recording
+def onRecordClick():                              
+    #changes the recording button between starting and stopping, as well as starts or stops recording
     if(recordButton['text']=="Start Recording"):
         recordButton['text']="Stop Recording"
         pauseButton.config(state = "normal")
@@ -113,10 +127,12 @@ def onRecordClick():                              #changes the recording button 
         saveButton.config(state = NORMAL)
 
 
-recordButton = Button(top, text = "Start Recording", command = onRecordClick, width = 15)  #creates the recording button
+recordButton = Button(top, text = "Start Recording", command = onRecordClick, width = 15)  
+#creates the recording button
 recordButton.place(x = 35,y = 50)
 
-def onPauseClick():                                #changes the pause button between pause and resume, as well as pauses or resumes recording
+def onPauseClick():                                
+    #changes the pause button between pause and resume, as well as pauses or resumes recording
     if(pauseButton['text']=="Pause Recording"):
         pauseButton['text']="Resume Recording"
         t.setOldTime()
@@ -129,52 +145,65 @@ def onPauseClick():                                #changes the pause button bet
         t.continueDisplay()
         resumeRecord()
 
-pauseButton = Button(top, text = "Pause Recording", command = onPauseClick, state = "disabled", width = 15)         #creates the pause button
+pauseButton = Button(top, text = "Pause Recording", command = onPauseClick, state = "disabled", width = 15)         
+#creates the pause button
 pauseButton.place(x=35, y = 80)
 
 CheckVar1 = IntVar()
-delayCheck = Checkbutton(top, text = "5s Delay", bg = "gray85", variable = CheckVar1, onvalue = 1, offvalue = 0)   #creates the delay check box
+delayCheck = Checkbutton(top, text = "5s Delay", bg = "gray85", variable = CheckVar1, onvalue = 1, offvalue = 0)   
+#creates the delay check box
 delayCheck.place(x = 35, y = 110)  
 
 CheckVar2 = IntVar()
-cameraCheck = Checkbutton(top, text = "Record Camera", bg = "gray85", variable = CheckVar2, onvalue = 1, offvalue = 0, state = DISABLED)   #creates the record camera button
+cameraCheck = Checkbutton(top, text = "Record Camera", bg = "gray85", variable = CheckVar2, onvalue = 1, offvalue = 0, state = DISABLED)   
+#creates the record camera button
 cameraCheck.place(x = 35, y = 135)
 
-cameraLabel = Label(top, text = "Camera Location:", bg = "gray85")       #Label for camera location
+cameraLabel = Label(top, text = "Camera Location:", bg = "gray85")       
+#Label for camera location
 cameraLabel.place(x = 35, y = 165)
 
-cameraDropMenu = ttk.Combobox(top, width = 14, values = ["Top-Left", "Top-Right", "Bottom-Left", "Bottom-Right"], state = "readonly")   #creates the drop down menu to select camera location
+cameraDropMenu = ttk.Combobox(top, width = 14, values = ["Top-Left", "Top-Right", "Bottom-Left", "Bottom-Right"], state = "readonly")   
+#creates the drop down menu to select camera location
 cameraDropMenu.current(0)
 cameraDropMenu.place(x = 37, y = 190)
 cameraDropMenu.bind("<FocusIn>",lambda x: recordOptions.focus())
 
-if video is None or not video.isOpened():     #if camera is not detected, this disables the record camera option
+if video is None or not video.isOpened():     
+    #if camera is not detected, this disables the record camera option
     cameraCheck.config(state = DISABLED)
 else:
     cameraCheck.config(state = NORMAL)
 
 #####################File Options####################
 
-fileLabel = Label(top, text = "File Options", font = "bold")          #creates the label text Record Option
+fileLabel = Label(top, text = "File Options", font = "bold")          
+#creates the label text Record Option
 fileLabel.place(x = 188, y = 20)
 
-fileOptions = Frame(top, width = 130, height = 180, bg = "gray85", highlightbackground="black", highlightthickness=1)     #creates a background frame to group the recording options together
+fileOptions = Frame(top, width = 130, height = 180, bg = "gray85", highlightbackground="black", highlightthickness=1)     
+#creates a background frame to group the recording options together
 fileOptions.place(x = 170, y = 45)
 
-saveLabel = Label(top, text = "Save Location:", bg = "gray85")          #creates the label text Record Option
+saveLabel = Label(top, text = "Save Location:", bg = "gray85")          
+#creates the label text Record Option
 saveLabel.place(x = 180, y = 85)
 
-locationText = Text(top, width = 14, height = 3)         #creates the textbox for showing the location where the mp4 will save
+locationText = Text(top, width = 14, height = 3)         
+#creates the textbox for showing the location where the mp4 will save
 locationText.place(x = 177, y = 110)
 filename = ""
 locationText.insert("end", filename)
-locationText.config(state = "disabled")    #have to enable textbox, set the location text, and then disable the text box to show it without the user being able to manually change the text
+locationText.config(state = "disabled")    
+#have to enable textbox, set the location text, and then disable the text box to show it without the user being able to manually change the text
 
-def saveLocation():                         #this will grab the filename and display it in the location Text box
+def saveLocation():                         
+    #this will grab the filename and display it in the location Text box
     global filename
     filename = filedialog.asksaveasfilename(title = "Select Save Location", filetypes = [("mp4 files", "*.mp4")]) 
     readLast = filename[-4:None]
-    if readLast != ".mp4":     #this will make sure the filename doesn't end with .mp4.mp4
+    if readLast != ".mp4":     
+        #this will make sure the filename doesn't end with .mp4.mp4
         filetype = ".mp4"
         filename += filetype
     locationText.config(state = "normal")
@@ -183,10 +212,12 @@ def saveLocation():                         #this will grab the filename and dis
     locationText.config(state = "disabled")
     locationText.see(END)
 
-locationButton = Button(top, text = "Choose Location", width = 14, command = saveLocation)  #creates the saving location button
+locationButton = Button(top, text = "Choose Location", width = 14, command = saveLocation)  
+#creates the saving location button
 locationButton.place(x = 180, y = 55)
 
-def mp4Save(): #this will combine the output video and sound file into an mp4 and save it to the proper location
+def mp4Save(): 
+    #this will combine the output video and sound file into an mp4 and save it to the proper location
     try:
         #https://www.reddit.com/r/learnpython/comments/ey41dp/merging_video_and_audio_using_ffmpegpython/
         ffmpeg.overwrite_output=True
@@ -197,22 +228,27 @@ def mp4Save(): #this will combine the output video and sound file into an mp4 an
     except:
         error = messagebox.showerror(title = "Error", message = "Please select a location, and make a recording first.")
 
-saveButton = Button(top, text = "Save MP4", font = "bold", width = 11, command = mp4Save)  #this will save the video and convert it to MP4
+saveButton = Button(top, text = "Save MP4", font = "bold", width = 11, command = mp4Save)  
+#this will save the video and convert it to MP4
 saveButton.place(x = 180, y = 180)
 
 ####################Timer Display####################
 
-timerDisplay = Frame(top, width = 272, height = 50, bg = "gray85", highlightbackground="black", highlightthickness=1)        #creates the timer frame
+timerDisplay = Frame(top, width = 272, height = 50, bg = "gray85", highlightbackground="black", highlightthickness=1)        
+#creates the timer frame
 timerDisplay.place(x = 28, y = 235)
 
-timerLabel = Label(top, text = "Recording Time: 0:0:0", font = "bold", bg = "gray85")        #sets up the timer label
+timerLabel = Label(top, text = "Recording Time: 0:0:0", font = "bold", bg = "gray85")        
+#sets up the timer label
 timerLabel.place(x = 80, y = 247)
 
 ####################Recording Preview Display######################
 
-recordImage = ImageTk.PhotoImage(Image.new('RGB', (426, 240), (0,0,0)))   # sets the size of the screen recording preview
+recordImage = ImageTk.PhotoImage(Image.new('RGB', (426, 240), (0,0,0)))   
+# sets the size of the screen recording preview
 
-def previewScreen():        #inputs each recorded frame of the screen for the screen preview
+def previewScreen():        
+    #inputs each recorded frame of the screen for the screen preview
     preview = True
     while preview:
         tempImg = pyautogui.screenshot()
@@ -220,32 +256,40 @@ def previewScreen():        #inputs each recorded frame of the screen for the sc
         tempFrame = cv2.resize(tempFrame, (426, 240))
         recordImage.paste(Image.fromarray(tempFrame))
 
-threading.Thread(target=previewScreen, daemon=True).start()   #threads the screen recording to run in the background
+threading.Thread(target=previewScreen, daemon=True).start()   
+#threads the screen recording to run in the background
 
-recordingPreview = Frame(top, width = 432, height = 246, highlightbackground = "black", highlightthickness = 1)    #creates the frame around the screen preview
+recordingPreview = Frame(top, width = 432, height = 246, highlightbackground = "black", highlightthickness = 1)    
+#creates the frame around the screen preview
 recordingPreview.place(x = 339, y = 44)
-recordingScreens = Label(top, width = 426, height = 240, image = recordImage)        #this is the label for where the screen preview goes
+recordingScreens = Label(top, width = 426, height = 240, image = recordImage)        
+#this is the label for where the screen preview goes
 recordingScreens.place(x = 340, y = 45)
 
 ####################Recording Audio Modules#######################
 class VoiceRecorder:
-    def __init__(self): #the constructor should take the filename as an argument for organization
+    def __init__(self): 
+        #Inintializes the VoiceRecorder
         self.audio_format = pyaudio.paInt16 
         self.channels = 1
         self.sample_rate = 44100 #44100 bits/second
         self.chunk = int(0.03*self.sample_rate)
 
     def audioRecord(self): 
-            self.recUser_data = [] 
-            self.p = pyaudio.PyAudio() #initializes pyaudio
-            self.stream = self.p.open(format=self.audio_format, channels=self.channels, rate=self.sample_rate, input=True, frames_per_buffer=self.chunk)
+        #https://realpython.com/playing-and-recording-sound-python/
+        #records the audio
+        self.recUser_data = [] 
+        self.p = pyaudio.PyAudio() 
+        #initializes pyaudio
+        self.stream = self.p.open(format=self.audio_format, channels=self.channels, rate=self.sample_rate, input=True, frames_per_buffer=self.chunk)
 
-            while recording:
-                data = self.stream.read(self.chunk)
-                self.recUser_data.append(data)
+        while recording:
+            data = self.stream.read(self.chunk)
+            self.recUser_data.append(data)
 
     def stopAudioRecording(self):
-            self.stream.stop_stream() #stops/closes stream
+            self.stream.stop_stream()
+            #stops/closes stream
             self.stream.close()
             self.p.terminate()
             self.recUser_data = [np.frombuffer(frame, dtype=np.int16) for frame in self.recUser_data]
@@ -256,7 +300,8 @@ voice = VoiceRecorder()
 
 ####################Recording Screen Modules######################
 
-def startRecording():                  #opens the screen recordings and starts threading the screenRecord method
+def startRecording():                  
+    #opens the screen recordings and starts threading the screenRecord method
     if not out.isOpened():
         out.open("output.avi", fourcc, 10, (SCREEN_SIZE))  
     global recording
@@ -264,9 +309,12 @@ def startRecording():                  #opens the screen recordings and starts t
     threading.Thread(target=screenRecord, daemon=True).start()
     threading.Thread(target=voice.audioRecord, daemon=True).start()
 
-def screenRecord():                    #Starts recording the screen
+def screenRecord():                    
+    #Starts recording the screen
     while recording:
-        if CheckVar2.get() == 1:        #records the camera and screen
+        if CheckVar2.get() == 1:        
+            #records the camera and screen
+            #https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_core/py_image_arithmetics/py_image_arithmetics.html#:~:text=You%20can%20add%20two%20images,just%20be%20a%20scalar%20value.
             img = pyautogui.screenshot()
             ret, videoImg = video.read()
             frame = np.array(img)
@@ -276,7 +324,8 @@ def screenRecord():                    #Starts recording the screen
                 rows2,cols2,channels2 = frame.shape
                 cols3 = cols2 - cols
                 rows3 = rows2 - rows
-            if ret is True:             #decides where to put the camera recording on the screen recording
+            if ret is True:             
+                #decides where to put the camera recording on the screen recording
                 if cameraDropMenu.get() == "Top-Left":
                     frame[0:rows, 0:cols] = videoImg
                 elif cameraDropMenu.get() == "Bottom-Left":
@@ -287,17 +336,20 @@ def screenRecord():                    #Starts recording the screen
                     frame[rows3:rows2, cols3:cols2] = videoImg
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
             time.sleep(1/90)
-        else:                            #records only the screen without the camera
+        else:                            
+            #records only the screen without the camera
             img = pyautogui.screenshot()
             frame = np.array(img)
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         out.write(frame)
 
-def pauseRecord():    #pauses the recording
+def pauseRecord():    
+    #pauses the recording
     global recording
     recording = False
 
-def resumeRecord():    #resumes the recording after screen
+def resumeRecord():    
+    #resumes the recording after screen
     global recording
     recording = True
     if not out.isOpened():
@@ -305,7 +357,8 @@ def resumeRecord():    #resumes the recording after screen
     threading.Thread(target=screenRecord, daemon=True).start()
     threading.Thread(target=voice.audioRecord, daemon=True).start()
 
-def stopRecording():    #stops the recording entirely and releases the video and camera recordings
+def stopRecording():    
+    #stops the recording entirely and releases the video and camera recordings
     global recording
     recording = False
     out.release()
